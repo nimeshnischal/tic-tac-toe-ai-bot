38fab42d200a1e2cd237ce1147af10f9eb4fd3b6
Nimesh Nischal <nimesh.nischal@livspace.com>
2020-10-15 17:52:47 +0530

    Updates

---
+++ precache-manifest.97271d873066b801c868abdeb41798d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92845680756e388612d18b5f10eb478e",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/index.html"
+  },
+  {
+    "revision": "614403c9745b20c5c770",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/css/2.593b1e63.chunk.css"
+  },
+  {
+    "revision": "1602e5dc571d3ef1cdc2",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/css/main.5b39b757.chunk.css"
+  },
+  {
+    "revision": "614403c9745b20c5c770",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/js/2.d8c1b7ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/js/2.d8c1b7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1602e5dc571d3ef1cdc2",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/js/main.e04f589f.chunk.js"
+  },
+  {
+    "revision": "35a4958c3c1c17dce743",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/js/runtime-main.fd66815f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nimeshnischal/tic-tac-toe-ai-bot/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
